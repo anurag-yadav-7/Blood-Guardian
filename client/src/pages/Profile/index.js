f0e87ee3d82b7ffe95b244489248bc6605db8cef
Anurag Yadav <92670570+anurag-yadav-7@users.noreply.github.com>
2024-07-10 15:00:03 +0530

    Update index.js

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import {Tabs} from 'antd';
 import Inventory from './Inventory';
-import Donars from './Donars';
+import donors from './donors';
 import Hospitals from './Hospitals';
 import Organizations from './Organizations';
 import InventoryTable from '../../components/InventoryTable';
@@ -18,8 +18,8 @@ function Profile() {
                 <Tabs.TabPane tab="Inventory" key="1"> 
                     <Inventory/>
                 </Tabs.TabPane>
-                <Tabs.TabPane tab="Donars" key = "2">
-                    <Donars/>    
+                <Tabs.TabPane tab="donors" key = "2">
+                    <donors/>    
                 </Tabs.TabPane>
                 <Tabs.TabPane tab = "Hospitals" key = "3"> 
                     <Hospitals/> 
@@ -27,19 +27,19 @@ function Profile() {
             </>
             }
 
-            {currentUser.userType === "donar" && 
+            {currentUser.userType === "donor" && 
                 <>
                     <Tabs.TabPane tab = "Donations" key = "4">
                         <InventoryTable
                             filters={{
                                 inventoryType: "in",
-                                donar: currentUser._id, 
+                                donor: currentUser._id, 
                             }}
-                            userType="donar"
+                            userType="donor"
                         />
                         </Tabs.TabPane>
                     <Tabs.TabPane tab = "Organizations" key = "5">
-                        <Organizations userType="donar"/>
+                        <Organizations userType="donor"/>
                     </Tabs.TabPane>
                 </>
             }
